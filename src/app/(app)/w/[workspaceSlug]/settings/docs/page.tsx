c88ee8b28dbfeb8f69bbdb9fd2d9a7b4ff8d6a24
felipecr-wlp <felipe.cr@pavific.com>
2026-08-04 13:14:04 -0600

    fix: _e en onKeyUp, eslint-disable en docs page

--- src/app/(app)/w/[workspaceSlug]/settings/docs/page.tsx
+++ src/app/(app)/w/[workspaceSlug]/settings/docs/page.tsx
@@ -5,6 +5,7 @@ import * as path from 'path'
 
 interface Props { params: { workspaceSlug: string } }
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export default async function DocsPage(_props: Props) {
   const supabase = createClient()
   const { data: { user } } = await supabase.auth.getUser()
